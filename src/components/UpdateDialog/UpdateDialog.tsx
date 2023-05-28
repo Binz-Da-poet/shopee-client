376314e01b0b23abf958a96ca8571c6e10b65838
Binz-Da-poet <19te468@gmail.com>
2023-05-28 21:48:52 +0900

    Delivery

---
+++ src/components/UpdateDialog/UpdateDialog.tsx
@@ -0,0 +1,79 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Button, Dialog, Card, CardBody, DialogHeader, DialogFooter } from '@material-tailwind/react'
+import { useForm } from 'react-hook-form'
+import Input from 'src/pages/AdminPage/Components/Input/Input'
+import { setPayPurchasesToLS } from 'src/utils/auth'
+import { UserSchema } from 'src/utils/rule'
+interface Props {
+  open: boolean
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+  shoppingCartId: number | undefined
+  purchaseIds: number[]
+  fullName: string
+  phoneNumber: string
+  address: string
+}
+const FormAddDialog = UserSchema.pick(['fullName', 'address', 'phoneNumber'])
+type FormDelivery = { fullName: string; address: string; phoneNumber: string }
+function UpdateDialog({ open, setOpen, shoppingCartId, purchaseIds, fullName, phoneNumber, address }: Props) {
+  const handleOpen = () => setOpen((cur) => !cur)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormDelivery>({
+    resolver: yupResolver(FormAddDialog),
+    defaultValues: {
+      fullName: fullName,
+      phoneNumber: phoneNumber,
+      address: address
+    }
+  })
+  const onSubmit = handleSubmit((data) => {
+    setPayPurchasesToLS(shoppingCartId, purchaseIds, data.address, data.fullName, data.phoneNumber)
+    setOpen(false)
+  })
+  return (
+    <Dialog size='sm' open={open} handler={handleOpen} className='fixed z-[1055] h-fit overflow-scroll '>
+      <Card>
+        <DialogHeader> Thêm địa chỉ nhận hàng</DialogHeader>
+        <CardBody className='flex flex-col p-2'>
+          <Input
+            labelText='Họ và tên'
+            type='text'
+            className='p-2'
+            name='fullName'
+            register={register}
+            errorMessage={errors.fullName?.message}
+          ></Input>
+          <Input
+            labelText='Điện thoại'
+            type='text'
+            className='p-2'
+            name='phoneNumber'
+            register={register}
+            errorMessage={errors.phoneNumber?.message}
+          ></Input>
+          <Input
+            type='address'
+            labelText='Địa chỉ nhận'
+            className='p-2'
+            name='address'
+            register={register}
+            errorMessage={errors.address?.message}
+          ></Input>
+        </CardBody>
+        <DialogFooter>
+          <Button variant='gradient' color='red' onClick={handleOpen} className='mr-1 rounded-lg bg-red-400 text-white'>
+            <span>Cancel</span>
+          </Button>
+          <Button variant='gradient' className='bg-blue-600' onClick={onSubmit}>
+            <span>Confirm</span>
+          </Button>
+        </DialogFooter>
+      </Card>
+    </Dialog>
+  )
+}
+
+export default UpdateDialog
